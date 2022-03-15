327ffd1286773d327b9e3318bacc253002682316
Kassio <kassio.barbosa@novaxs.com.br>
2022-03-15 15:17:30 -0300

    Fix(Babel config/prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:vue/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+}
